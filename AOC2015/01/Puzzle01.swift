6cf7ea19efd08587cef95e31ece4f387cc72d302
Jaydeep Joshi <jaydeepmjoshi@gmail.com>
2021-08-01 18:32:41 +0530

    Solutions for 01

--- AOC2015/01/Puzzle01.swift
+++ AOC2015/01/Puzzle01.swift
@@ -7,11 +7,39 @@
 
 struct Puzzle01: Puzzle {
 
+    private let input: String
+
+    init() {
+        input = InputFileReader.read("Input01")[0]
+    }
+
     func part1() {
-        
+        let answer = input.reduce(into: 0) { floor, c in
+            if c == "(" {
+                floor += 1
+            }
+            if c == ")" {
+                floor -= 1
+            }
+        }
+        print(answer)
     }
 
     func part2() {
-
+        var answer = 0
+        var floor = 0
+        for (index, c) in input.enumerated() {
+            if c == "(" {
+                floor += 1
+            }
+            if c == ")" {
+                floor -= 1
+                if floor == -1 {
+                    answer = (index + 1)
+                    break
+                }
+            }
+        }
+        print(answer)
     }
 }
