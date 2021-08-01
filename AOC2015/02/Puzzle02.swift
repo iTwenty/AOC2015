f8dd6306c42ee2e897911b360fd05a24f2422051
Jaydeep Joshi <jaydeepmjoshi@gmail.com>
2021-08-01 19:01:15 +0530

    Solutions for 02

--- AOC2015/02/Puzzle02.swift
+++ AOC2015/02/Puzzle02.swift
@@ -5,13 +5,45 @@
 //  Created by Jaydeep Joshi on 31/07/21.
 //
 
+fileprivate typealias Dimensions = (l: Int, w: Int, h: Int)
+
 struct Puzzle02: Puzzle {
 
+    private let input: [Dimensions]
+
+    init() {
+        input = InputFileReader.read("Input02").map({ line in
+            let ss = line.split(separator: "x")
+            return (Int(ss[0])!, Int(ss[1])!, Int(ss[2])!)
+        })
+    }
+
     func part1() {
-        
+        let answer = input.reduce(0) { acc, d in
+            acc + paperArea(for: d)
+        }
+        print(answer)
     }
 
     func part2() {
+        let answer = input.reduce(0) { acc, d in
+            acc + ribbonLength(for: d)
+        }
+        print(answer)
+    }
 
+    private func paperArea(for d: Dimensions) -> Int {
+        let lw = d.l*d.w
+        let wh = d.w*d.h
+        let hl = d.h*d.l
+        let slack = [lw, wh, hl].sorted()[0]
+        return 2*lw + 2*wh + 2*hl + slack
+    }
+
+    private func ribbonLength(for d: Dimensions) -> Int {
+        let twoSmallestSides = [d.l, d.w, d.h].sorted().dropLast()
+        let perimeter = 2 * (twoSmallestSides[0] + twoSmallestSides[1])
+        let ribbon = d.l * d.w * d.h
+        return perimeter + ribbon
     }
 }
