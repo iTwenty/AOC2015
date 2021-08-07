522e8b278e20aa64cff8c116d0fe77d8a7138830
Jaydeep Joshi <jaydeepmjoshi@gmail.com>
2021-08-07 19:58:44 +0530

    Solutions for 10

--- AOC2015/10/Puzzle10.swift
+++ AOC2015/10/Puzzle10.swift
@@ -7,11 +7,28 @@
 
 struct Puzzle10: Puzzle {
 
+    private let input = "1113122113"
+
     func part1() {
-        
+        print(lookAndSay(start: input, count: 40).count)
     }
 
     func part2() {
+        print(lookAndSay(start: input, count: 50).count)
+    }
 
+    private func lookAndSay(start: String, count: Int) -> String {
+        var current = start
+        for _ in (0..<count) {
+            let chunked = current.chunked { $0 == $1 }
+            var new = ""
+            chunked.forEach { str in
+                let count = str.count
+                let char = str.first!
+                new.append("\(count)\(char)")
+            }
+            current = new
+        }
+        return current
     }
 }
