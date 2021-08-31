566fc87760d2c271cc10256278adcb934e3f35f9
Jaydeep Joshi <jaydeepmjoshi@gmail.com>
2021-08-31 10:47:20 +0530

    Solutions for 25

--- AOC2015/25/Puzzle25.swift
+++ AOC2015/25/Puzzle25.swift
@@ -5,13 +5,33 @@
 //  Created by Jaydeep Joshi on 31/07/21.
 //
 
+fileprivate typealias Position = (row: Int, col: Int)
+
 struct Puzzle25: Puzzle {
+    private let targetPosition = (2947, 3029)
 
     func part1() {
-        
+        let targetIndex = index(forPosition: targetPosition)
+        var code = 20151125
+        for _ in 2...targetIndex {
+            code = nextCode(code)
+        }
+        print(code)
     }
 
     func part2() {
+        // Ooooweeee!
+    }
 
+    private func nextCode(_ current: Int) -> Int {
+        (current * 252533) % 33554393
+    }
+
+    private func index(forPosition position: Position) -> Int {
+        let diagonal = position.row + position.col - 1
+        let diagonalEndIndex = diagonal * (diagonal + 1) / 2
+        let diagonalStartIndex = diagonalEndIndex - diagonal + 1
+        let index = diagonalStartIndex + (diagonal - position.row)
+        return index
     }
 }
